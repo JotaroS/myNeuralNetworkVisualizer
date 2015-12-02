d604d2f479f1c99eedd7a9296bc04c0150bd5dba
Jotaro Shigeyama <jojojoe77@gmail.com>
2015-12-02 11:36:31 +0900

    added pdf code

--- graph.m
+++ graph.m
@@ -1,9 +1,10 @@
 
 
-M=csvread('out2.csv');
+M=csvread('out.csv');
 xlabel("number of interm layer")
 ylabel("iteration")
 plot(M(1,:),M(2,:),"linewidth",3.0,'color',[1.0 0 0])
+legend('0%')
 hold on;
 plot(M(1,:),M(3,:),"linewidth",3.0,'color',[0.9 0 0])
 hold on;
@@ -22,7 +23,10 @@ hold on;
 plot(M(1,:),M(10,:),"linewidth",3.0,'color',[0.2 0 0])
 hold on;
 plot(M(1,:),M(11,:),"linewidth",3.0,'color',[0.1 0 0])
-hold on;
-
+hold off;
+L=legend({'10%','20%','30%','40%','50%','60%','70%','80%','90%'},'location','northeastoutside');
+title("Iteration count for different interm layer and input noise rate",'FontSize',15);
+xlabel('Number of interm layer','FontSize',15)
+ylabel('Count of iteration','FontSize',15)
 input('quit')
-print -deps file2.eps
+print -dpdf file.pdf
